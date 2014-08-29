906b22fbd6667a00d28c1900a5c0086b36232470
Spencer Dixon <spencercdixon@gmail.com>
2014-08-29 16:05:18 -0400

    Put ajax back on

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,14 +2,14 @@ var $alertBox = $('.alert-box')
 $alertBox.delay(3000).fadeOut(1000)
 
 
-// $(function() {
-//
-//   $('#project').on('submit', function(e){
-//     e.preventDefault();
-//     var details = $('#project').serialize();
-//     $.post('/profile/:user_id/projects', details, function(data){
-//       $('#profile_project').html(data);
-//     });
-//   });
-//
-// });
+$(function() {
+
+  $('#project').on('submit', function(e){
+    e.preventDefault();
+    var details = $('#project').serialize();
+    $.post('/profile/:user_id/projects', details, function(data){
+      $('#profile_project').html(data);
+    });
+  });
+
+});
