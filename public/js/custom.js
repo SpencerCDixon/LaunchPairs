1669305f2ef4c38946d084e23474b355167399bc
Spencer Dixon <spencercdixon@gmail.com>
2014-08-31 17:06:31 -0400

    updated ajax

--- public/js/custom.js
+++ public/js/custom.js
@@ -25,3 +25,27 @@ $(function() {
   });
 
 });
+
+$(function() {
+
+  $('#project').on('submit', function(e){
+    e.preventDefault();
+
+    // var details = $('#project').serializeArray();
+
+    // $.post('/profile/:user_id/projects', details, function(data){
+    //   $('#profile_project').html(data);
+    // });
+
+    $.ajax({
+      type: 'post',
+      dataType: 'json',
+      data: $('#project').serialize(),
+      url: '/profile/:user_id/projects',
+      success: function(data) {
+        $('#profile_project').text(data.project);
+      }
+    });
+  });
+
+});
