581a8c36f5b619cea357fa4aa3e5323ba3e7376c
Spencer Dixon <spencercdixon@gmail.com>
2014-11-11 08:37:39 -0500

    updated schema to allow quick reset with heroku

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
-# Adds data into status table
-INSERT INTO status (user_id, status, created_at) VALUES (2,'doing something',now());
+-- # Adds data into status table
+-- INSERT INTO status (user_id, status, created_at) VALUES (2,'doing something',now());
+--
+-- # Get the most recent status from someone?
+-- SELECT DISTINCT ON users.id FROM users INNER JOIN status ON users.id=status.id ORDER BY users.id, status.date DESC LIMIT 1;
+--
+-- # Finds the most recent status available based on ID
+-- select * from status WHERE id = 2 order by created_at desc limit 1;
 
-# Get the most recent status from someone?
-SELECT DISTINCT ON users.id FROM users INNER JOIN status ON users.id=status.id ORDER BY users.id, status.date DESC LIMIT 1;
 
-# Finds the most recent status available based on ID
-select * from status WHERE id = 2 order by created_at desc limit 1;
-
-
-##### NEW DATABASE DESIGN PREPARATION #########
+-- ##### NEW DATABASE DESIGN PREPARATION #########
 CREATE TABLE users(
   id serial PRIMARY KEY,
   uid varchar(255) NOT NULL,
